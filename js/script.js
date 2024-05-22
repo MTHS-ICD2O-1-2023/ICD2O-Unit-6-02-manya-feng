83137a6bcfea05887a111d266465c63b9545a82a
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-22 22:21:35 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+* Check servie worker.
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2OR-Unit6-02-HTML/sw.js", {
+    scope: "/ICD2OR-Unit6-02-HTML",
+  })
+}
+
+function cookies() {
+  if (localStorage.hits) {
+    localStorage.hits++
+  } else {
+    localStorage.hits = 1
+  }
+
+  document.getElementById("total").innerHTML =
+    "The time that you click is " + localStorage.hits + "!"
+}
+
